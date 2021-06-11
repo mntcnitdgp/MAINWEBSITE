fd70464847c9c9a7aa42adcd1b64bcd13e3189af
Karam Sahoo <karamsahoo@pop-os.localdomain>
2021-06-11 11:04:21 +0530

    navbar added

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,58 @@
 import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const Footer = () => {
     return (
-        <div>
-            <p>Copyright 2021 MNTC</p>
-        </div>
+        <footer className="site-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-6">
+                        <h6>About</h6>
+                        <p className="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+                    </div>
+
+                    <div className="col-xs-6 col-md-3">
+                        <h6>Categories</h6>
+                        <ul className="footer-links">
+                            <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+                            <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+                            <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+                            <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+                            <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+                            <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-xs-6 col-md-3">
+                        <h6>Quick Links</h6>
+                        <ul className="footer-links">
+                            <li><Link href="/about"><a>About Us</a></Link></li>
+                            <li><Link href="/events"><a>Our Events</a></Link></li>
+                            <li><Link href="/about"><a>Contact Us</a></Link></li>
+                            <li><Link href="/team"><a>Meet the team</a></Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-sm-6 col-xs-12">
+                        <p className="copyright-text">Copyright &copy; 2021 All Rights Reserved by
+                                <a href="#"> MNTC</a>.
+                            </p>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6 col-xs-12">
+                        <ul className="social-icons">
+                            <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+                            <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+                            <li><a className="dribbble" href="#"><i className="fa fa-dribbble"></i></a></li>
+                            <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,52 @@
 import Link from 'next/link'
+import { useState } from 'react'
+import styles from '../styles/Nav.module.css'
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
     return (
-        <div>
-            <h2>MNTC</h2>
-            <ul>
-                <li><Link href='/'>Home</Link></li>
-                <li><Link href='/about'>About</Link></li>
-                <li><Link href='/team'>Team</Link></li>
-            </ul>
-        </div>
+        <div className={styles.Nav}>
+            <div className={styles.NavContainer}>
+
+                <div className={styles.menu_icon} onClick={() => setClicked(!clicked)} >
+                    <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    <p>MENU</p>
+                </div>
+
+                <ul className={clicked ? styles.NavMenu + " " + styles.active : styles.NavMenu}>
+                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+                        <Link className={styles.NavLinks} href='/'>
+                            Home
+                        </Link>
+                    </li>
+
+                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+                        <Link className={styles.NavLinks} href='/about'>
+                            About
+                        </Link>
+                    </li>
+
+                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+                        <Link className={styles.NavLinks} href='/events'>
+                            Events
+                        </Link>
+                    </li>
+
+                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+                        <Link className={styles.NavLinks} href='/team'>
+                            Team
+                        </Link>
+                    </li>
+
+                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+                        <Link className={styles.NavLinks} href='/contact'>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div >
     )
 }
 
