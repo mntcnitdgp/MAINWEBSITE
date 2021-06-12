c5a53e118c0c2b99e3e76af32ca4988a47933390
A2nurag <anurag2sarkar@gmail.com>
2021-06-12 23:30:33 +0530

    Design Changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,63 +1,49 @@
-import Link from 'next/link'
-import { useState } from 'react'
-import styles from '../styles/Nav.module.css'
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
+import Link from "next/link";
+import { useState } from "react";
+import styles from "../styles/Nav.module.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import Image from "next/image";
 
 const Navbar = () => {
-    const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-    return (
-        <div className={styles.Nav}>
-            <div className={styles.NavContainer}>
+  return (
+    <div className={styles.Nav}>
+      <div className={"container" + " " + styles.NavContainer}>
+        <div className={styles.NavLogo}>
+          <img src="/images/logo.svg" alt="logo"></img>
+        </div>
 
-                <div className={styles.NavLogo}>
-                    <h2>MNTC</h2>
-                </div>
+        <div className={styles.menu_icon} onClick={() => setClicked(!clicked)}>
+          {!clicked ? <MenuIcon /> : <CloseIcon />}
+        </div>
 
-                <div className={styles.menu_icon} onClick={() => setClicked(!clicked)} >
-                    {
-                        !clicked ?
-                            <MenuIcon /> : <CloseIcon style={{ color: "black" }} />
+        <nav
+          className={
+            clicked
+              ? `${styles.NavMenu} ${styles.active} bg-card-light`
+              : styles.NavMenu
+          }
+        >
+          <NavLinks name="Home" />
+          <NavLinks name="Our Team" />
+          <NavLinks name="Events" />
+          <NavLinks name="Contact Us" />
+        </nav>
+        <div style={{ flex: 0.5 }}></div>
+      </div>
+    </div>
+  );
+};
 
-                    }
-
-                </div>
-
-                <ul className={clicked ? styles.NavMenu + " " + styles.active : styles.NavMenu}>
-                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-                        <Link className={styles.NavLinks} href='/'>
-                            <a>Home</a>
-                        </Link>
-                    </li>
-
-                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-                        <Link className={styles.NavLinks} href='/about'>
-                            <a>About</a>
-                        </Link>
-                    </li>
-
-                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-                        <Link className={styles.NavLinks} href='/events'>
-                            <a>Events</a>
-                        </Link>
-                    </li>
-
-                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-                        <Link className={styles.NavLinks} href='/team'>
-                            <a>Team</a>
-                        </Link>
-                    </li>
-
-                    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-                        <Link className={styles.NavLinks} href='/contact'>
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </div >
-    )
-}
-
+const NavLinks = ({ name }) => {
+  return (
+    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+      <Link href="/">
+        <a className={`color-header-text ${styles.NavLinks}`}>{name}</a>
+      </Link>
+    </li>
+  );
+};
 export default Navbar;
--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,18 @@
-import Link from 'next/link'
-import styles from '/home/karamsahoo/Desktop/mntc-website/styles/Notfound.module.css'
+import Link from "next/link";
+// import styles from '/home/karamsahoo/Desktop/mntc-website/styles/Notfound.module.css'
 
 const notFound = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Not Found</h1>
-            <p>Go back to the <Link href='/'><a>Home Page</a></Link></p>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <h1>Not Found</h1>
+      <p>
+        Go back to the{" "}
+        <Link href="/">
+          <a>Home Page</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
 
 export default notFound;
