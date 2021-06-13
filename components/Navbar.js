acf6707c252cbc8a1ea3a71d59f6af6a543e02eb
A2nurag <anurag2sarkar@gmail.com>
2021-06-13 08:01:52 +0530

    Fixed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,10 +7,9 @@ import Image from "next/image";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
-
   return (
     <div className={styles.Nav}>
-      <div className={"container" + " " + styles.NavContainer}>
+      <div className={`container ${styles.NavContainer}`}>
         <div className={styles.NavLogo}>
           <img src="/images/logo.svg" alt="logo"></img>
         </div>
@@ -21,15 +20,31 @@ const Navbar = () => {
 
         <nav
           className={
-            clicked
-              ? `${styles.NavMenu} ${styles.active} bg-card-light`
-              : styles.NavMenu
+            clicked ? `${styles.NavMenu} ${styles.active}` : styles.NavMenu
           }
         >
-          <NavLinks name="Home" />
-          <NavLinks name="Our Team" />
-          <NavLinks name="Events" />
-          <NavLinks name="Contact Us" />
+          <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+            <Link href="/">
+              <a className={`color-header-text ${styles.NavLinks}`}>Home</a>
+            </Link>
+          </li>
+
+          <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+            <Link href="/team">
+              <a className={`color-header-text ${styles.NavLinks}`}>Our Team</a>
+            </Link>
+          </li>
+
+          <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+            <Link href="/events">
+              <a className={`color-header-text ${styles.NavLinks}`}>Events</a>
+            </Link>
+          </li>
+          <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
+            <Link href="/about">
+              <a className={`color-header-text ${styles.NavLinks}`}>About Us</a>
+            </Link>
+          </li>
         </nav>
         <div style={{ flex: 0.5 }}></div>
       </div>
@@ -37,13 +52,4 @@ const Navbar = () => {
   );
 };
 
-const NavLinks = ({ name }) => {
-  return (
-    <li className={styles.NavItems} onClick={() => setClicked(!clicked)}>
-      <Link href="/">
-        <a className={`color-header-text ${styles.NavLinks}`}>{name}</a>
-      </Link>
-    </li>
-  );
-};
 export default Navbar;
