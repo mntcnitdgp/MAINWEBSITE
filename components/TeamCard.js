7069cfb7fd0ad23453c06b76ed4bb11a92db1547
A2nurag <anurag2sarkar@gmail.com>
2021-06-17 22:32:42 +0530

    Team Page

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,23 +1,111 @@
-import React from 'react'
-import styles from '../styles/Team.module.css'
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import React from "react";
+import styles from "../styles/Team.module.css";
+import FeatherIcon from "feather-icons-react";
+import Image from "next/image";
+function TeamCard({
+  name,
+  avatar,
+  position,
+  facebook,
+  instagram,
+  linkedin,
+  github,
+  twitter,
+}) {
+  return (
+    <div className={styles.TeamCard}>
+      {position && (
+        <p className={`${styles.DetailsPosition} ${styles.PositionDesktop}`}>
+          {position}
+        </p>
+      )}
+      <div className={styles.TeamCardAvatarContainer}>
+        {avatar && <Image src={avatar} width="100" height="100" />}
+      </div>
 
-function TeamCard({ name, position }) {
-    return (
-        <div className={styles.card}>
-            <div className={styles.bg}></div>
-            <p className={styles.position}>{position}</p>
-            <div className={styles.avatar}></div>
-            <p className={styles.name}>{name}</p>
-            <div className={styles.soc}>
-                <FacebookIcon style={{ fontSize: 30 }} />
-                <InstagramIcon style={{ fontSize: 30 }} />
-                <LinkedInIcon style={{ fontSize: 30 }} />
-            </div>
+      <div className={styles.TeamCardDetailsContainer}>
+        <h3 className={styles.DetailsName}>{name}</h3>
+        {position && <p className={styles.DetailsPosition}>{position}</p>}
+
+        <div className={styles.TeamCardSocial}>
+          {facebook && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={facebook}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="facebook"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
+
+          {instagram && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={instagram}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="instagram"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
+
+          {linkedin && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={linkedin}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="linkedin"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
+
+          {github && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={github}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="github"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
+
+          {twitter && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={twitter}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="twitter"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default TeamCard
+export default TeamCard;
--- pages/team/index.js
+++ pages/team/index.js
@@ -1,47 +1,187 @@
-import { team } from '../../data/team/finalYear';
-import Head from 'next/head'
-import TeamCard from '../../components/TeamCard';
-import styles from '../../styles/Team.module.css'
-import { motion } from 'framer-motion';
+import { year4 } from "../../data/team/year4";
+import { year3 } from "../../data/team/year3";
+import { year2 } from "../../data/team/year2";
+import { year1 } from "../../data/team/year1";
 
-const Team = ({ teamList }) => {
-    return (
-        <motion.div className={styles.teamPage} initial="pageInitial" animate="pageAnimate" variants={{
-            pageInitial: {
-                opacity: 0
-            },
-            pageAnimate: {
-                opacity: 1,
-                transition: {
-                    duration: 1
-                }
-            },
-        }}
-            exit={{ opacity: 0 }}>
-            <Head>
-                <title>MNTC | Our Team</title>
-                <meta name="description" content="Blah Blah Blah" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+import Head from "next/head";
+import TeamCard from "../../components/TeamCard";
+import styles from "../../styles/Team.module.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-            <h1 className={styles.subheader}>The Valence Electrons</h1>
+const Team = () => {
+  const [year, setYear] = useState(4);
 
-            <div className={styles.team_cards_container}>
-                {teamList.map((member) => (
-                    <TeamCard key={member.id} name={member.name} position={member.position} />
-                ))}
-            </div>
-        </motion.div>
-    )
+  let team =
+    year == 1 ? (
+      <Year1 />
+    ) : year == 2 ? (
+      <Year2 />
+    ) : year == 3 ? (
+      <Year3 />
+    ) : (
+      <Year4 />
+    );
+
+  return (
+    <div className={styles.Team}>
+      <div className={`container ${styles.TeamContainer}`}>
+        <div className={styles.YearSwitch}>
+          <span
+            className={
+              year == 4
+                ? `${styles.Year} ${styles.Year4} ${styles.YearActive}`
+                : `${styles.Year} ${styles.Year4}`
+            }
+            onClick={() => setYear(4)}
+          >
+            Fourth Year
+          </span>
+          <span
+            className={
+              year == 3
+                ? `${styles.Year} ${styles.Year3} ${styles.YearActive}`
+                : `${styles.Year} ${styles.Year3}`
+            }
+            onClick={() => setYear(3)}
+          >
+            Third Year
+          </span>
+          <span
+            className={
+              year == 2
+                ? `${styles.Year} ${styles.Year2} ${styles.YearActive}`
+                : `${styles.Year} ${styles.Year2}`
+            }
+            onClick={() => setYear(2)}
+          >
+            Second Year
+          </span>
+          <span
+            className={
+              year == 1
+                ? `${styles.Year} ${styles.Year1} ${styles.YearActive}`
+                : `${styles.Year} ${styles.Year1}`
+            }
+            onClick={() => setYear(1)}
+          >
+            First Year
+          </span>
+        </div>
+        {team}
+      </div>
+    </div>
+  );
+};
+
+function Year1() {
+  return (
+    <>
+      <h1 className={`${styles.TeamHeader} font-header color-header-text`}>
+        Juniors Who Havent Been to College
+      </h1>
+      <div className={styles.TeamList}>
+        {year1.map((member) => {
+          return (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              avatar={member.avatar}
+              position={member.position}
+              facebook={member.fb}
+              instagram={member.insta}
+              linkedin={member.link}
+              github={member.git}
+              twitter={member.twit}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export const getStaticProps = async () => {
-    return {
+function Year2() {
+  return (
+    <>
+      <h1 className={`${styles.TeamHeader} font-header color-header-text`}>
+        Give a Better Name for Second Years
+      </h1>
+      <div className={styles.TeamList}>
+        {year2.map((member) => {
+          return (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              avatar={member.avatar}
+              position={member.position}
+              facebook={member.facebook}
+              instagram={member.instagram}
+              linkedin={member.linkedin}
+              github={member.github}
+              twitter={member.twitter}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
 
-        props: {
-            teamList: team
-        }
-    }
+function Year3() {
+  return (
+    <>
+      <h1 className={`${styles.TeamHeader} font-header color-header-text`}>
+        Give a Better Name for Third Years
+      </h1>
+      <div className={styles.TeamList}>
+        {console.log("year3")}
+        {year3.map((member) => {
+          return (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              avatar={member.avatar}
+              position={member.position}
+              facebook={member.facebook}
+              instagram={member.instagram}
+              linkedin={member.linkedin}
+              github={member.github}
+              twitter={member.twitter}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+function Year4() {
+  return (
+    <>
+      <h1 className={`${styles.TeamHeader} font-header color-header-text`}>
+        Our Ultimate heroes
+      </h1>
+      <div className={styles.TeamList}>
+        {console.log("year4")}
+        {year4.map((member) => {
+          return (
+            <TeamCard
+              key={member.id}
+              name={member.name}
+              avatar={member.avatar}
+              position={member.position}
+              facebook={member.facebook}
+              instagram={member.instagram}
+              linkedin={member.linkedin}
+              github={member.github}
+              twitter={member.twitter}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Team;
