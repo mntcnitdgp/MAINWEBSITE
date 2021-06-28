20908525943dbe0dfeaa14db9e22f496a3e3ee86
A2nurag <anurag2sarkar@gmail.com>
2021-06-29 01:30:40 +0530

    lots and lots of changes part1

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,111 +1,84 @@
-import React from "react";
 import styles from "../styles/Team.module.css";
 import FeatherIcon from "feather-icons-react";
 import Image from "next/image";
+
+import FacebookLogo from "./Logos/FacebookLogo";
+import InstagramLogo from "./Logos/InstagramLogo";
+import LinkedinLogo from "./Logos/LinkedinLogo";
 function TeamCard({
-    name,
-    avatar,
-    position,
-    facebook,
-    instagram,
-    linkedin,
-    github,
-    twitter,
+  name,
+  avatar,
+  position,
+  facebook,
+  instagram,
+  linkedin,
+  github,
+  twitter,
 }) {
-    return (
-        <div className={styles.TeamCard}>
-            {position && (
-                <p className={`${styles.DetailsPosition} ${styles.PositionDesktop}`}>
-                    {position}
-                </p>
-            )}
-            <div className={styles.TeamCardAvatarContainer}>
-                {avatar && <Image src={avatar} width="100" height="100" />}
-            </div>
+  return (
+    <div className={`Card ${styles.TeamCard}`}>
+      {position && (
+        <p className={`${styles.DetailsPosition} ${styles.PositionDesktop}`}>
+          {position}
+        </p>
+      )}
+      <div className={styles.TeamCardAvatarContainer}>
+        {avatar && <Image src={avatar} width="100" height="100" />}
+      </div>
 
-            <div className={styles.TeamCardDetailsContainer}>
-                <h3 className={styles.DetailsName}>{name}</h3>
-                {position && <p className={styles.DetailsPosition}>{position}</p>}
+      <div className={styles.TeamCardDetailsContainer}>
+        <h3 className={styles.DetailsName}>{name}</h3>
+        {position && <p className={styles.DetailsPosition}>{position}</p>}
 
-                <div className={styles.TeamCardSocial}>
-                    {facebook && (
-                        <a
-                            className={styles.TeamCardSocialLinks}
-                            href={facebook}
-                            target="_blank"
-                        >
-                            <FeatherIcon
-                                icon="facebook"
-                                size="24"
-                                stroke="none"
-                                fill="hsla(0, 0%, 100%, 0.75)"
-                            />
-                        </a>
-                    )}
+        <div className={styles.TeamCardSocial}>
+          {facebook && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={facebook}
+              target="_blank"
+            >
+              <FacebookLogo color="hsla(10, 50%, 97%, 1)" />
+            </a>
+          )}
 
-                    {instagram && (
-                        <a
-                            className={styles.TeamCardSocialLinks}
-                            href={instagram}
-                            target="_blank"
-                        >
-                            <FeatherIcon
-                                icon="instagram"
-                                size="24"
-                                stroke="none"
-                                fill="hsla(0, 0%, 100%, 0.75)"
-                            />
-                        </a>
-                    )}
+          {instagram && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={instagram}
+              target="_blank"
+            >
+              <InstagramLogo color="hsla(10, 50%, 97%, 1)" />
+            </a>
+          )}
 
-                    {linkedin && (
-                        <a
-                            className={styles.TeamCardSocialLinks}
-                            href={linkedin}
-                            target="_blank"
-                        >
-                            <FeatherIcon
-                                icon="linkedin"
-                                size="24"
-                                stroke="none"
-                                fill="hsla(0, 0%, 100%, 0.75)"
-                            />
-                        </a>
-                    )}
+          {linkedin && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={linkedin}
+              target="_blank"
+            >
+              <LinkedinLogo color="hsla(10, 50%, 97%, 1)" />
+            </a>
+          )}
 
-                    {github && (
-                        <a
-                            className={styles.TeamCardSocialLinks}
-                            href={github}
-                            target="_blank"
-                        >
-                            <FeatherIcon
-                                icon="github"
-                                size="24"
-                                stroke="none"
-                                fill="hsla(0, 0%, 100%, 0.75)"
-                            />
-                        </a>
-                    )}
-
-                    {twitter && (
-                        <a
-                            className={styles.TeamCardSocialLinks}
-                            href={twitter}
-                            target="_blank"
-                        >
-                            <FeatherIcon
-                                icon="twitter"
-                                size="24"
-                                stroke="none"
-                                fill="hsla(0, 0%, 100%, 0.75)"
-                            />
-                        </a>
-                    )}
-                </div>
-            </div>
+          {github && (
+            <a
+              className={styles.TeamCardSocialLinks}
+              href={github}
+              target="_blank"
+            >
+              <FeatherIcon
+                icon="github"
+                size="24"
+                stroke="none"
+                fill="hsla(0, 0%, 100%, 0.75)"
+              />
+            </a>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default TeamCard;
--- pages/events/index.js
+++ pages/events/index.js
@@ -9,57 +9,59 @@ import { useState } from "react";
 import FeatherIcon from "feather-icons-react";
 
 export default function Events() {
-    const [details, setDetails] = useState(events[0]);
-    const [active, setActive] = useState(false);
+  const [details, setDetails] = useState(events[0]);
+  const [active, setActive] = useState(false);
+  return (
+    <motion.div
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+          transition: {
+            duration: 1,
+          },
+        },
+      }}
+      exit={{ opacity: 0 }}
+    >
+      <Head>
+        <title>MNTC | Events</title>
+        <meta name="description" content="Blah Blah Blah" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`container ${styles.eventContainer}`}>
+        <h2 className={styles.header}>Events round the calendar</h2>
+        <div className={styles.EventList}>
+          {events.map((event) => {
+            return <EventCard key={event.id} details={event} />;
+          })}
+        </div>
+      </div>
+    </motion.div>
+  );
+
+  function EventCard(props) {
+    const { id, name, image, desc, startDate, endDate, link } = props.details;
+    const date = startDate == endDate ? startDate : `${startDate} - ${endDate}`;
     return (
-        <motion.div
-            initial="pageInitial"
-            animate="pageAnimate"
-            variants={{
-                pageInitial: {
-                    opacity: 0,
-                },
-                pageAnimate: {
-                    opacity: 1,
-                    transition: {
-                        duration: 1,
-                    },
-                },
-            }}
-            exit={{ opacity: 0 }}
-        >
-            <Head>
-                <title>MNTC | Events</title>
-                <meta name="description" content="Blah Blah Blah" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={`container ${styles.eventContainer}`}>
-                <h1 className={styles.header}>Events round the calendar</h1>
-                <div className={styles.EventList}>
-                    {events.map((event) => {
-                        return <EventCard key={event.id} details={event} />;
-                    })}
-                </div>
-            </div>
-        </motion.div>
+      <div className={styles.EventCard}>
+        {console.log(props)}
+        <div className={styles.EventPosterContainer}>
+          <Image src={image} alt={name} src={image} layout="fill"></Image>
+        </div>
+
+        <div className={styles.EventDetails}>
+          <h3 className={styles.DetailsName}>{name}</h3>
+          <p className={styles.DetailsDate}>{date}</p>
+          <Link href={`/events/${name}?` + `index=${id}`}>
+            <p>Know More</p>
+          </Link>
+        </div>
+      </div>
     );
-
-    function EventCard(props) {
-        const { id, name, image, desc, startDate, endDate, link } = props.details;
-        const date = startDate == endDate ? startDate : `${startDate} - ${endDate}`;
-        return (
-            <div className={styles.EventCard}>
-                {console.log(props)}
-                <div className={styles.EventPosterContainer}>
-                    <Image src={image} alt={name} src={image} layout="fill"></Image>
-                </div>
-
-                <div className={styles.EventDetails}>
-                    <h3 className={styles.DetailsName}>{name}</h3>
-                    <p className={styles.DetailsDate}>{date}</p>
-                    <Link href={`/events/${name}?` + `index=${id}`}><p>Know More</p></Link>
-                </div>
-            </div>
-        );
-    }
+  }
 }
