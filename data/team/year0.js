3169c95cdd75cf449aae17f2f3c9e93b34ec784f
KaramSahoo <51832871+KaramSahoo@users.noreply.github.com>
2021-08-15 11:53:50 +0530

    Update year0.js

--- data/team/year0.js
+++ data/team/year0.js
@@ -2,18 +2,18 @@ const relativePath = "/images/member images/year 0/"; //Change this on deploy
 export const year0 = [
   {
     id: 1,
-    name: "Anita Pal",
+    name: "Dr. Anita Pal",
     position: "Associate Professor",
   },
 
   {
     id: 2,
-    name: "Sarit Maitra",
+    name: "Dr. Sarit Maitra",
     position: "Associate Professor",
   },
   {
     id: 3,
-    name: "Seema Sarkar (Mondal)",
+    name: "Dr. Seema Sarkar (Mondal)",
     position: "Professor",
   },
 ];
