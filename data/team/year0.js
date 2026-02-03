977128790bc8e7b4ef24f0b55d6b722376ed2e4c
Saraswatsahoo <ssahoo.code.13@gmail.com>
2026-02-04 01:17:37 +0530

    change facaulty advisor

--- data/team/year0.js
+++ data/team/year0.js
@@ -2,22 +2,29 @@ const relativePath = "/images/member images/year 0/";
 export const year0 = [
   {
     id: 1,
-    name: "Dr. Anita Pal",
-    position: "Associate Professor",
-    avatar: `${relativePath}anita.jpg`,
-  },
-
-  {
-    id: 3,
-    name: "Dr. Seema Sarkar (Mondal)",
-    position: "Professor",
-    avatar: `${relativePath}seema.jpg`,
-  },
-
-  {
-    id: 2,
     name: "Dr. Lakshmi Kanta Dey",
     position: "Associate Professor",
     avatar: `${relativePath}lakshmi.jpg`,
   },
+
+  {
+    id: 2,
+    name: "Dr. Pinaki Pal",
+    position: "Associate Professor",
+    avatar: `${relativePath}pinaki.jpg`,
+  },
+
+  // {
+  //   id: 3,
+  //   name: "Dr. Anita Pal",
+  //   position: "Associate Professor",
+  //   avatar: `${relativePath}anita.jpg`,
+  // },
+
+  // {
+  //   id: 4,
+  //   name: "Dr. Seema Sarkar (Mondal)",
+  //   position: "Professor",
+  //   avatar: `${relativePath}seema.jpg`,
+  // },
 ];
